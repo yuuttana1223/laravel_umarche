74010d20aa3ffc285c59a6d3e74f1e2a64505acd
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-20 13:48:43 +0900

    名前付きスロット

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -19,6 +19,9 @@
 </head>
 
 <body>
+    <header>
+        {{ $header }}
+    </header>
     <div class="font-sans text-gray-900 antialiased">
         {{ $slot }}
     </div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">ヘッダー2</x-slot>
     コンポーネント2
 </x-tests.app>
