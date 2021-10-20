ac61b63a13324453feb081395944994152b480fb
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-20 14:19:04 +0900

    変数

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -8,7 +8,8 @@ class ComponentTestController extends Controller
 {
     public function showComponent1()
     {
-        return view('tests.component-test1');
+        $message = 'メッセージ';
+        return view('tests.component-test1', compact('message'));
     }
     public function showComponent2()
     {
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,5 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネント1
-    <x-tests.card title="タイトル" content="本文" />
+    <x-tests.card title="タイトル" content="本文" :message="$message" />
 </x-tests.app>
