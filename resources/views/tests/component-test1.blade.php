cb6ca24d657017e96cfbb26d2a5e82a16d1a12b6
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-20 14:30:37 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネント1
-    <x-tests.card title="タイトル" content="本文" :message="$message" />
+    <x-tests.card title="タイトル1" content="本文" :message="$message" />
+    <x-tests.card title="タイトル2" />
 </x-tests.app>
