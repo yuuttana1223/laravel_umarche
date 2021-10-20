0d201ffced28678540711152c6acc691a18f0a5f
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-20 14:42:57 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,6 +1,8 @@
 @props(['title', 'message' => '初期値です', 'content' => '本文初期値です'])
 
-<div class="border-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2',
+]) }}>
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
