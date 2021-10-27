d4e3fe6e7147b6420062e1759c854d0b8294722f
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-28 00:49:10 +0900

    image seeder

---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 3; $i++) {
+            DB::table('images')->insert([
+                'owner_id' => 1,
+                'filename' => "sample{$i}.jpg",
+                'title' => null,
+            ]);
+        }
+    }
+}
