14e6107e35b8d553b7f3fccb5104a02240e5e868
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-28 00:49:10 +0900

    image seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
+            ImageSeeder::class,
         ]);
     }
 }
