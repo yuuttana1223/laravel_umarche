a2008cbea13099a149cc8e92ae02da901341e4fd
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-11-12 12:31:08 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemsController.php
+++ app/Http/Controllers/User/ItemsController.php
@@ -9,6 +9,17 @@ use Illuminate\Support\Facades\DB;
 
 class ItemsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+            $product = Product::findOrFail($request->route('item'));
+            if ($product->is_selling === 0) {
+                abort(404);
+            }
+            return $next($request);
+        })->only('show');
+    }
+
     public function index(Request $request)
     {
         $products = Product::availableItems()->get();
